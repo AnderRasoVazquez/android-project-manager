abec2fd49be97b68b0a8338b8b56fed7afe72766
Ander Raso Vázquez <anderraso@gmail.com>
2019-04-14 16:14:47 +0200

    pruebas token iniciales
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- app/src/main/java/com/example/projectmanager/view/login/LoginActivity.java
+++ app/src/main/java/com/example/projectmanager/view/login/LoginActivity.java
@@ -3,11 +3,13 @@ package com.example.projectmanager.view.login;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,9 +24,18 @@ import com.example.projectmanager.view.register.RegisterActivity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.channels.FileChannel;
 
 /**
@@ -66,16 +77,19 @@ public class LoginActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-                boolean success = DB.getInstance(getApplicationContext()).login(loginString);
+                conexionBDWebService c = new conexionBDWebService();
+                c.execute();
 
-                if (!success) {
-                    Toast toast = Toast.makeText(getApplicationContext(), R.string.cantLogin, Toast.LENGTH_SHORT);
-                    toast.show();
-                } else {
-                    Intent intent = new Intent(LoginActivity.this, ProjectsActivity.class);
-                    intent.putExtra("email", email);
-                    startActivity(intent);
-                }
+//                boolean success = true;
+//
+//                if (!success) {
+//                    Toast toast = Toast.makeText(getApplicationContext(), R.string.cantLogin, Toast.LENGTH_SHORT);
+//                    toast.show();
+//                } else {
+//                    Intent intent = new Intent(LoginActivity.this, ProjectsActivity.class);
+//                    intent.putExtra("email", email);
+//                    startActivity(intent);
+//                }
             }
         });
 
@@ -120,6 +134,63 @@ public class LoginActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private class conexionBDWebService extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... args) {
+//            String server = "http://192.168.1.128:5000";
+            String server = "https://proyecto-das.herokuapp.com";
+            String apiLogin = "/api/v1/login";
+            String direccion = server + apiLogin;
+            String authString = "admin:admin";
+            byte[] authStringEnc = Base64.encode(authString.getBytes(), Base64.DEFAULT);
+            String basicAuth = "Basic " + new String(authStringEnc);
+
+            HttpURLConnection urlConnection = null;
+            URL destino = null;
+            try {
+                destino = new URL(direccion);
+                urlConnection = (HttpURLConnection) destino.openConnection();
+                urlConnection.setConnectTimeout(5000);
+                urlConnection.setReadTimeout(5000);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty ("Authorization", basicAuth);
+                int statusCode = urlConnection.getResponseCode();
+                System.out.println("el status code" + statusCode);
+                InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                String response = convertInputStreamToString(inputStream);
+                JSONObject json = new JSONObject(response);
+                System.out.println(json.toString(4));
+//                PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+//                out.print(parametrosJSON.toString());
+//                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            System.out.println("On post execute: " + s);
+            super.onPostExecute(s);
+        }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 
 }
